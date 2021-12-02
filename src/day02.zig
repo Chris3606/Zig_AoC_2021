5e48fe6b5b8f1f5eb7ce930af1d40879450b0639
Chris3606 <cridley106@gmail.com>
2021-12-02 14:50:46 -0600

    Completed day 2.

--- src/day02.zig
+++ src/day02.zig
@@ -11,8 +11,74 @@ const gpa = util.gpa;
 
 const data = @embedFile("../data/day02.txt");
 
+const Point = struct {
+    x: i32 = 0,
+    y: i32 = 0,
+};
+
+const MoveCommand = union(enum) {
+    up_down: i32,
+    forward: i32,
+};
+
+fn followCommandsPt1(commands: []const MoveCommand) Point {
+    var point = Point{};
+
+    for (commands) |command| {
+        switch (command) {
+            .forward => |val| point.x += val,
+            .up_down => |val| point.y += val,
+        }
+    }
+
+    return point;
+}
+
+fn followCommandsPt2(commands: []const MoveCommand) Point {
+    var aim: i32 = 0;
+    var position = Point{};
+
+    for (commands) |command| {
+        switch (command) {
+            .forward => |val| {
+                position.x += val;
+                position.y += (aim * val);
+            },
+            .up_down => |val| aim += val,
+        }
+    }
+
+    return position;
+}
+
 pub fn main() !void {
-    
+    defer if (util.gpa_impl.deinit()) unreachable;
+
+    var commands = List(MoveCommand).init(gpa);
+    defer commands.deinit();
+
+    var it = tokenize(u8, data, "\n");
+    while (it.next()) |line| {
+        var line_it = tokenize(u8, line, " ");
+        const dir = line_it.next().?;
+        const val = try parseInt(i32, line_it.next().?, 10);
+
+        if (std.mem.eql(u8, dir, "forward")) {
+            try commands.append(.{ .forward = val });
+        } else if (std.mem.eql(u8, dir, "down")) {
+            try commands.append(.{ .up_down = val });
+        } else if (std.mem.eql(u8, dir, "up")) {
+            try commands.append(.{ .up_down = -val });
+        } else unreachable;
+    }
+
+    // Part 1
+    const end_pos_pt1 = followCommandsPt1(commands.items);
+    print("Part 1: {d}, mult: {d}\n", .{ end_pos_pt1, end_pos_pt1.x * end_pos_pt1.y });
+
+    // Part 2
+    const end_pos_pt2 = followCommandsPt2(commands.items);
+    print("Part 2: {d}, mult: {d}\n", .{ end_pos_pt2, end_pos_pt2.x * end_pos_pt2.y });
 }
 
 // Useful stdlib functions
